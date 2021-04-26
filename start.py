aa86ccc55c47f0e0d6c4a7c4ce3097944fa7fe5d
Shota <shamashukeli@gmail.com>
2021-04-26 12:58:49 +0300

    yas

--- start.py
+++ start.py
@@ -21,21 +21,25 @@ def run_rf(X_train, X_test, y_train, y_test, random_state=0):
     y_probas_test = clf.predict_proba(X_test)[:, 1]
     roc_train = roc_auc_score(y_train, y_probas_train)
     roc_test = roc_auc_score(y_test, y_probas_test)
-    return roc_train, roc_test
+    return np.array((roc_train, roc_test))
 
-def run_test(data_path,target_col):
+def run_test(data_path,target_col,runs=1):
     print("--------- Running: {} ---------".format(data_path))
     X_train, X_test, y_train, y_test = load_data(data_path,target_col)
     print('Raw:')
-    rs = run_rf(X_train, X_test, y_train, y_test)
+    rs = 0
+    for i in range(runs):
+        rs += run_rf(X_train, X_test, y_train, y_test,random_state=i)
+    rs /= runs
     print(rs)
     print('BigFeat:')
-    bf = bigfeat.BigFeat()
-    X_train_p = bf.fit(X_train, y_train)
-    X_train = bf.produce(X_train)
-    X_test = bf.produce(X_test)
-    #return 0
-    rs = run_rf(X_train, X_test, y_train, y_test)
+    rs = 0
+    for i in range(runs):
+        bf = bigfeat.BigFeat()
+        X_train = bf.fit(X_train, y_train,random_state=i)
+        X_test = bf.produce(X_test)
+        rs += run_rf(X_train, X_test, y_train, y_test,random_state=i)
+    rs /= runs
     print(rs)
 
 
