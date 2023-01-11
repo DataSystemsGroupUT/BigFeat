be40bf9dc641b2491ea2949cfd788d7e84a23ca4
Hassan Eldeeb <Hassan-Eldeeb@users.noreply.github.com>
2023-01-11 10:47:31 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,21 +1,20 @@
 from setuptools import setup
 
-
 setup(
+    description = ("Automated feature engineering library"),
     name = "bigfeat",
     version = "0.1",
-    #author = "Shota Amashukeli, Hassan Eldeeb, Radwa Elshawi",
+    #author = "Hassan Eldeeb, Shota Amashukeli, Radwa Elshawi",
     #author_email = "firstName.lastName@ut.ee",
-    description = ("Automated feature engineering library"),
     license = "MIT",
     keywords = "feature engineering", "machine learning", "automl", "feature extraction", "feature selection",
     url = "https://github.com/DataSystemsGroupUT/BigFeat",
     packages=['bigfeat'],
     long_description=("Automated feature engineering library"),
-          install_requires=[
-          'pandas',
-          'numpy',
-          'scikit-learn',
-          'lightgbm',
-      ],
+    install_requires=[
+        'pandas',
+        'numpy',
+        'scikit-learn',
+        'lightgbm',
+    ],
 )
