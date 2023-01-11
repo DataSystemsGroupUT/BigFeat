457fd3569430e27a4afcc39c5a65af95c0cd1436
Hassan Eldeeb <Hassan-Eldeeb@users.noreply.github.com>
2023-01-11 10:54:41 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
     #author = "Hassan Eldeeb, Shota Amashukeli, Radwa Elshawi",
     #author_email = "firstName.lastName@ut.ee",
     license = "MIT",
-    keywords = "feature engineering", "machine learning", "automl", "feature extraction", "feature selection",
+    keywords = ["feature engineering", "machine learning", "automl", "feature extraction", "feature selection"],
     url = "https://github.com/DataSystemsGroupUT/BigFeat",
     packages=['bigfeat'],
     long_description=("Automated feature engineering library"),
